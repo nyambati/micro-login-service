930958e0e651b80cdca6ba2e44702e22304c64ea
DhulkifliHussein <sir3nkali@DhulkifliHusseins-MacBook-Pro.local>
2018-05-18 19:50:25 +0300

    feat(routes): setup routes - setup routes why: we need endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, except: [:new, :edit] do
+    collection do
+      post 'login'
+      get 'generate_access_token'
+    end
+  end
+
+  get "auth/:provider/callback", to: 'users#google_login'
+  get 'auth/failure', to: redirect('/')
+
+  post 'password/forgot', to: 'passwords#forgot'
+
+  root to: "users#index"
+
+  resources :invites, only: [:index, :create]
+  resources :roles, only: [:index, :show, :create, :destroy, :update]
 end
