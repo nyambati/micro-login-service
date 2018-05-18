f0a93ea2a182ed6d9c95aa4417b9e6b07c1bb4d8
DhulkifliHussein <sir3nkali@DhulkifliHusseins-MacBook-Pro.local>
2018-05-18 20:09:24 +0300

    chore(concerns): setup exception handlers - setup exception handlers why: we need to handle exceptions gracefuly

---
+++ app/controllers/concerns/exception_handler.rb
@@ -0,0 +1,20 @@
+module ExceptionHandler
+  extend ActiveSupport::Concern
+
+  included do
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActionController::ParameterMissing, with: :parameter_missing_response
+    def render_unprocessable_entity_response(e)
+      json_response({ message: e.record.errors }, :unprocessable_entity)
+    end
+
+    def render_not_found_response(e)
+      json_response({ error: e.exception }, :not_found)
+    end
+
+    def parameter_missing_response(e)
+      json_response({ error: e.exception }, :unprocessable_entity)
+    end
+  end
+end
