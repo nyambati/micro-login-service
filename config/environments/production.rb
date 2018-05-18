23d144792ff4ecead44411bfdf7980be23be9097
DhulkifliHussein <sir3nkali@DhulkifliHusseins-MacBook-Pro.local>
2018-05-18 20:24:53 +0300

    feat(setupMailers): setup mail config - setup mail config for our email why: we have mailing views we now - :have to configure them so that they can be sent out

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,21 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "andela.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
+
+  config.action_mailer.default_url_options = { host: "#{ENV["DOMAIN"]}" }
+
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
